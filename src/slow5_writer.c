79f8cd1bec6a407f62a13fa92e9ffdbd7aa04427
NidhiPaalpare <nidhilakshmipaalpare@gmail.com>
2022-07-14 18:58:40 +1000

    basic implementation of slow5_open, commented out other functions because they aren't referenced by the slow5.c file in the master branch, and won't execute when we checkout to write_apir

--- src/slow5_writer.c
+++ src/slow5_writer.c
@@ -10,24 +10,43 @@
 int slow5_writer(char *output_path, rec_t *pod5_data_records) {
 
     printf("in slow5 writer %s\n", output_path);
-    int batch_row_count = 1000;
-    //process and print (time not measured as we want to compare to the time it takes to read the file)
-    double *sums = (double*)malloc(batch_row_count * sizeof(double));
-    // #pragma omp parallel for
-    for(int i=0;i<batch_row_count;i++){
-        uint64_t sum = 0;
-        //looking through records 
-        for(int j=0; (unsigned)j<pod5_data_records[i].len_raw_signal; j++){
-            sum +=  ((pod5_data_records[i].raw_signal[j] + pod5_data_records[i].offset) * pod5_data_records[i].scale);
-        }
-        sums[i] = sum;
-    }
-    //printing in row format to stdout -- this is possiblly where we want to output our struct
-    for(int i=0;i<batch_row_count;i++){
-        fprintf(stdout,"%s\t%f\n",pod5_data_records[i].read_id,sums[i]); 
-    }
-    free(sums);
-    fprintf(stderr,"batch printed with %d reads\n",batch_row_count);
+	// if file already exists
+	//if (access(output_path, F_OK) == -1) {
+
+	slow5_file_t *sp = slow5_open(output_path, "w");
+	if(sp==NULL){
+		fprintf(stderr,"Error opening file!\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	//slow5_hdr_t* header = sp->header;
+	//example header add
+	//slow5_hdr_add("run_id", header);
+
+	//slow5_hdr_set("run_id", "0", 0, header);
+
+	//slow5_hdr_write(sp);
+
+
+
+    // int batch_row_count = 1000;
+    // //process and print (time not measured as we want to compare to the time it takes to read the file)
+    // double *sums = (double*)malloc(batch_row_count * sizeof(double));
+    // // #pragma omp parallel for
+    // for(int i=0;i<batch_row_count;i++){
+    //     uint64_t sum = 0;
+    //     //looking through records 
+    //     for(int j=0; (unsigned)j<pod5_data_records[i].len_raw_signal; j++){
+    //         sum +=  ((pod5_data_records[i].raw_signal[j] + pod5_data_records[i].offset) * pod5_data_records[i].scale);
+    //     }
+    //     sums[i] = sum;
+    // }
+    // //printing in row format to stdout -- this is possiblly where we want to output our struct
+    // for(int i=0;i<batch_row_count;i++){
+    //     fprintf(stdout,"%s\t%f\n",pod5_data_records[i].read_id,sums[i]); 
+    // }
+    // free(sums);
+    // fprintf(stderr,"batch printed with %d reads\n",batch_row_count);
 
     return 0;
 }
--- src/slow5_writer.h
+++ src/slow5_writer.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include "pod5_reader.h"
 #include <slow5/slow5.h>
 
