fced7e19d2e928cfb7f75c924a41f5164a7e2d5e
NidhiPaalpare <nidhilakshmipaalpare@gmail.com>
2022-07-24 19:14:29 +1000

    incorporated a welcome message

--- src/main.c
+++ src/main.c
@@ -14,8 +14,18 @@
 int main (int argc, char *argv[]) {
 
     if (argc != 3) {
+		if (argc == 1) {
+			printf("\n************************************************************\n");
+			printf("\tWelcome to the Pod5->Slow5 converter 1.0.0!!\t\n");
+			printf("************************************************************\n\n");
+			printf("Usages:\n./p2s infile.pod5 outfile.slow5\n./p2s infile.pod5 outfile.blow5\n\n");
+			printf("infile: path to user's existing pod5 file\n");
+			printf("outfile: path to a slow5 or blow5 file named by the user which is created via the converter\n\n");
+			exit(0);
+		} else {
         fprintf(stderr, "Usage: %s in_file.pod5 out_file.slow5\n", argv[0]);
         return EXIT_FAILURE;
+		}
     }
 
     int ret = pod5_reader(argc, argv);
