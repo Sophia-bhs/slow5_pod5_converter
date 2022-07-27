113fcbba7177b0c5b8fad1845eb606009a7287c2
NidhiPaalpare <nidhilakshmipaalpare@gmail.com>
2022-07-27 18:17:32 +1000

    adding -h/--help command

--- src/main.c
+++ src/main.c
@@ -14,14 +14,15 @@
 int main (int argc, char *argv[]) {
 
     if (argc != 3) {
-		if (argc == 1) {
+		if (strcmp(argv[1], "--help") || strcmp(argv[1], "-h")) {
 			printf("\n************************************************************\n");
 			printf("\tWelcome to the Pod5->Slow5 converter 1.0.0!!\t\n");
 			printf("************************************************************\n\n");
-			printf("Usages:\t./p2s infile.pod5 outfile.slow5\n\t./p2s infile.pod5 outfile.blow5\n\n");
-			printf("infile:  path to user's existing pod5 file\n\n");
-			printf("outfile: path to a slow5 or blow5 file named by\n");
-			printf("\t the user which is created via the converter\n\n");
+			printf("Usages:\t./p2s [OPTION] [INPUT FILE] [OUTPUT FILE]\n\n");
+			printf("OPTIONS:\n\t-h, --help\tDisplay this message and exit.\n\n");
+			printf("INPUT FILE:\n\tPath to user's existing pod5 file.\n\n");
+			printf("OUTPUT FILE:\n\tPath to a slow5 or blow5 file named by\n");
+			printf("\tthe user which is created via the converter.\n\n");
 			exit(0);
 		} else {
 			fprintf(stderr, "Usage: %s in_file.pod5 out_file.slow5\n", argv[0]);
