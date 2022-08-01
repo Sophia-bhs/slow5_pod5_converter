7544c0c514d57d21697aa2630bf5af219b23e3db
Hiruna Samarakoon <hiruna72@gmail.com>
2022-08-01 13:42:51 +1000

    prevent seg fault when no argument is passed to p2s.

--- src/main.c
+++ src/main.c
@@ -14,20 +14,20 @@
 int main (int argc, char *argv[]) {
 
     if (argc != 3) {
-		if (strcmp(argv[1], "--help") || strcmp(argv[1], "-h")) {
-			printf("\n************************************************************\n");
-			printf("\tWelcome to the Pod5->Slow5 converter 1.0.0!!\t\n");
-			printf("************************************************************\n\n");
-			printf("Usage:\t./p2s [OPTION] [INPUT FILE] [OUTPUT FILE]\n\n");
-			printf("OPTIONS:\n\t-h, --help\tDisplay this message and exit.\n\n");
-			printf("INPUT FILE:\n\tPath to user's existing pod5 file.\n\n");
-			printf("OUTPUT FILE:\n\tPath to a slow5 or blow5 file named by\n");
-			printf("\tthe user which is created via the converter.\n\n");
-			exit(0);
-		} else {
-			fprintf(stderr, "Usage: %s in_file.pod5 out_file.slow5\n", argv[0]);
-			return EXIT_FAILURE;
-		}
+		// if (strcmp(argv[1], "--help") || strcmp(argv[1], "-h")) {
+		printf("\n************************************************************\n");
+		printf("\tWelcome to the Pod5->Slow5 converter 1.0.0!!\t\n");
+		printf("************************************************************\n\n");
+		printf("Usage:\t./p2s [OPTION] [INPUT FILE] [OUTPUT FILE]\n\n");
+		printf("OPTIONS:\n\t-h, --help\tDisplay this message and exit.\n\n");
+		printf("INPUT FILE:\n\tPath to user's existing pod5 file.\n\n");
+		printf("OUTPUT FILE:\n\tPath to a slow5 or blow5 file named by\n");
+		printf("\tthe user which is created via the converter.\n\n");
+		exit(0);
+		// } else {
+		// 	fprintf(stderr, "Usage: %s in_file.pod5 out_file.slow5\n", argv[0]);
+		// 	return EXIT_FAILURE;
+		// }
     }
 
     int ret = pod5_reader(argc, argv);
